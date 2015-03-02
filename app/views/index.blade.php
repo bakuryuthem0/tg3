09930560267679ab7d428bf9d5c099a9156ece26
Carlucho <shenlong_12@hotmailcom>
2015-03-01 21:32:49 -0430

    banda nueva

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -24,10 +24,11 @@
 					</div>
 				</div>			
 			</div>	
-			<div id="mini_gif">
-				<img class="separator" src="{{URL::to('images/nini-banner.gif')}}">
-			</div>
+			
 		</div>
+	</div>
+	<div id="mini_gif">
+		<img class="separator" src="{{URL::to('images/nini-banner.gif')}}">
 	</div>
 </div>
 <div id="about" class="row contenedorGrande">
@@ -38,75 +39,116 @@
 			</div>
 		</div>
 	</div>
-	<div id="banda">
-			<div class="cont_palabra">
-				<div id="top">
-					<div class="medio">
-						<p class="palabra col-xs-6 textoPromedio" id="c">
-							Constancia
-						</p>
-						<p  class="palabra col-xs-6 textoPromedio" id="r">
-							Responsabilidad
-						</p>
+	<div id="banda" class="row">
+			<div class="cont_palabra col-xs-12">
+				<div id="top" class="col-xs-12">
+					<div class="col-xs-12" >
+						<div class="col-xs-6 medio textLeft">
+							<p class="palabra col-xs-6 textoPromedio" id="c">
+								Constancia
+							</p>
+							<p  class="palabra col-xs-6 textoPromedio" id="r">
+								Responsabilidad
+							</p>
+						</div>
+						
 					</div>
-					<div class="medio">
-						<p class="palabra col-xs-6 textoPromedio" id="g">
-							Gratificación
-						</p>
-						<p class="palabra col-xs-6 textoPromedio" id="i">
-							Innovación
-						</p>
+					<div class="col-xs-12" >
+						<div class="col-xs-6 medio textMid">
+							<p class="palabra col-xs-6 textoPromedio" id="g">
+								Gratificación
+							</p>
+							<p class="palabra col-xs-6 textoPromedio" id="i">
+								Innovación
+							</p>
+						</div>
 					</div>
-					<div class="medio">
-						<p class="palabra col-xs-6 textoPromedio" id="t">
-							Tecnología
-						</p>
-						<p class="palabra col-xs-6 textoPromedio" id="a">
-							Autenticidad
-						</p>
+					<div class="col-xs-12">
+						<div class="col-xs-6 medio textRight">
+							<p class="palabra col-xs-6 textoPromedio" id="t">
+								Tecnología
+							</p>
+							<p class="palabra col-xs-6 textoPromedio" id="a">
+								Autenticidad
+							</p>
+						</div>
 					</div>
 				</div>
 				<div style="clear:both;"></div>
-				<div id="mid">
-					<h1 class="letra" id="T">T</h1>
-					<h1 class="letra" id="E">E</h1>
-					<h1 class="letra" id="C">C</h1>
-					<h1 class="letra" id="N">N</h1>
-					<h1 class="letra" id="O">O</h1>
-					<h1 class="letra" id="G">G</h1>
-					<h1 class="letra" id="R">R</h1>
-					<h1 class="letra" id="A">A</h1>
-					<h1 class="letra" id="P">P</h1>
-					<h1 class="letra" id="H">H</h1>
-					<h1 class="letra" id="I">I</h1>
-					<h1 class="letra" id="K">C</h1>
-				</div>
-				<div id="bot">
-					<div class="medio">
-						<p class="palabra col-xs-4 textoPromedio" id="e">
-							Evolución
-						</p>
-						<p class="palabra col-xs-4 textoPromedio" id="o">
-							Organización
-						</p>
-						<p class="palabra col-xs-4 textoPromedio" id="k">
-							Crecimiento
-						</p>
+				<div class="col-xs-12">
+					<div class="medio col-xs-6 textMid">
+						<h1 class="letra" id="T">T</h1>
+						<h1 class="letra" id="E">E</h1>
+						<h1 class="letra" id="C">C</h1>
+						<h1 class="letra" id="N">N</h1>
+						<h1 class="letra" id="O">O</h1>
+						<h1 class="letra" id="G">G</h1>
+						<h1 class="letra" id="R">R</h1>
+						<h1 class="letra" id="A">A</h1>
+						<h1 class="letra" id="P">P</h1>
+						<h1 class="letra" id="H">H</h1>
+						<h1 class="letra" id="I">I</h1>
+						<h1 class="letra" id="K">C</h1>
 					</div>
-					<div class="medio">
-						<p class="palabra col-xs-4 textoPromedio" id="n">
-							Negociación
-						</p>
-						<p class="palabra col-xs-4 textoPromedio" id="p">
-							Profesionalismo
-						</p>
-						<p class="palabra col-xs-4 textoPromedio" id="h">
-							Humildad
-						</p>
+				</div>
+				
+				<div id="bot" class="col-xs-12">
+					<div class="col-xs-12">
+						<div class="col-xs-6 medio textRight">
+							<p class="palabra col-xs-4 textoPromedio" id="e">
+								Evolución
+							</p>
+							<p class="palabra col-xs-4 textoPromedio" id="o">
+								Organización
+							</p>
+							<p class="palabra col-xs-4 textoPromedio" id="k">
+								Crecimiento
+							</p>
+						</div>
+						
+					</div>
+					<div class="col-xs-12">
+						<div class="col-xs-6 medio textLeft">
+							<p class="palabra col-xs-4 textoPromedio" id="n">
+								Negociación
+							</p>
+							<p class="palabra col-xs-4 textoPromedio" id="p">
+								Profesionalismo
+							</p>
+							<p class="palabra col-xs-4 textoPromedio" id="h">
+								Humildad
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
 			
+		</div>
+		<div class="container">
+				<div class="row about">
+					<div class="col-xs-6">
+						<div class="about1 ab2 textoPromedio">
+							<h3><strong>Tecnographic Venezuela</strong></h3>
+							<p>Somos una Agencia publicitaria y consultora responsable dedicada a la publicidad digital,  diseño de páginas web, tiendas virtuales, campañas de email, redes sociales y  desarrollo  de sistemas administrativos. </p>
+							<p>
+							Nos enfocamos en brindar a nuestros clientes  un servicio  de alto grado de funcionalidad,  interactividad, de manera que el sitio web se convierta en una verdadera herramienta para hacer negocios online, y que se adapte a su  necesidad.
+							</p>
+							<p>En<strong>Tecnography</strong>
+							 de Venezuela desarrollamos e integramos soluciones tecnológicas basadas en la sistematización y en las nuevas tecnologías, utilizando las últimas aplicaciones existentes en el mercado. Construimos el futuro, dotando a nuestros clientes de las herramientas para competir en la nueva era.
+						</div>
+					</div>
+					<div class="col-xs-6">
+						<div class="about1 textoPromedio">
+							<h3>Nuestra Meta</h3>
+							<p>Nuestra meta es lograr consolidar la empresa en el mercado de la tecnología web, brindando servicio  a la mayor cantidad de clientes en el menor tiempo posible. Dar soporte técnico y actualización de páginas desarrolladas. Ser reconocidos local, nacional e internacionalmente.</p>
+							<h3>Nuestro Equipo</h3>
+							<p>Nuestro equipo de profesionales está formado por especialistas en distintas áreas como Diseño Grafico, Ingeniería en sistemas, Programación, Comunicación Social, y Community Manager.</p>
+						</div>
+					</div>
+				</div>
+		</div>
+		<div id="refran">
+			<img src="{{URL::to('images/refran-01.png')}}">
 		</div>
 </div>
 @stop
--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,22 @@
 jQuery(document).ready(function($) {
 	$("html").niceScroll();
 });
+$('.letra').hover(function()
+{
+	var id = $(this).attr('id').toLowerCase();
+	var esto = $('#'+id);
+	esto.animate({
+		opacity: '1'
+	},500)
+},
+function()
+{
+	var id = $(this).attr('id').toLowerCase();
+	var esto = $('#'+id);
+	esto.animate({
+		opacity: '0'
+	},500)
+})
 /*$(document).ready(function ()
 {
 	$("html").niceScroll();
